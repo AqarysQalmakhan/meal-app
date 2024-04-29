d5d83a9d58b46bae045346b74d40a84f263edaaa
AqarysQalmakhan <qalmakhanoff@icloud.com>
2024-04-30 01:14:21 +0500

    ok

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,13 +1,25 @@
+import banner from './assets/Banner block.png'
+import Header from './components/Header.jsx'
+import Restaurants from './components/Restaurants.jsx'
+import Footer from './components/Footer.jsx'
+
+
 import './App.css'
-import MyButton from './components/Mybutton'
 
 function App() {
+  return (
+    <>
+    <Header />
+    <img src={banner} alt="" />
+    <h2>Our favourite picks</h2>
+    <Restaurants />
+    <h2>New arrivals</h2>
+    <Restaurants />
+    <h2>Categories</h2>
+    <Footer />
+    
+    </>
+  )
+}
 
-  return 
-  <div>
-    <MyButton> Первая кнопка </MyButton>
-
-    <MyButton>Вторая кнопка</MyButton>
-  </div>
-   }
 export default App
